67aa2fa6c0a756ffb1f9a58ad823fafb29e7976a
Virginia Wu <79432798+VirginiaWu11@users.noreply.github.com>
2021-10-02 18:45:25 -0400

    document function

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,6 +1,13 @@
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
+/**
+ * Helper function to help conver Javascript style variables into actual databse column names to be used in an sql query.
+ *
+ * @param  dataToUpdate {Object} user passes in fields to update{field1: newVal,field2:newVal, ...}
+ * @param  jsToSql {object} maps js style data fields to database column names. e.g. { firstName: "first_name", age: "age" }
+ * @returns {Object} {SQL column names separated by commas,values}
+ * e.g {["first_name=$1","last_name=$2"], ["John", "Doe"]}
+ */
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
     const keys = Object.keys(dataToUpdate);
