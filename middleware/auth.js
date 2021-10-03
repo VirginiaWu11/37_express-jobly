4c118e8df2bfc8d9e15244464ce7fac2a890109c
Virginia Wu <79432798+VirginiaWu11@users.noreply.github.com>
2021-10-02 20:36:19 -0400

    update authorization for users

--- middleware/auth.js
+++ middleware/auth.js
@@ -49,9 +49,23 @@ function ensureAdmin(req, res, next) {
         return next(err);
     }
 }
+function ensureCorrectUserOrAdmin(req, res, next) {
+    try {
+        const user = res.locals.user;
+        if (
+            !(user && (user.isAdmin || user.username === req.params.username))
+        ) {
+            throw new UnauthorizedError();
+        }
+        return next();
+    } catch (err) {
+        return next(err);
+    }
+}
 
 module.exports = {
     authenticateJWT,
     ensureLoggedIn,
     ensureAdmin,
+    ensureCorrectUserOrAdmin,
 };
