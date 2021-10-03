4b29e0f896c9b4dc16a7700330b21a608779d65f
Virginia Wu <79432798+VirginiaWu11@users.noreply.github.com>
2021-10-02 20:30:44 -0400

    refactor admin function

--- middleware/auth.js
+++ middleware/auth.js
@@ -42,7 +42,8 @@ function ensureLoggedIn(req, res, next) {
 }
 function ensureAdmin(req, res, next) {
     try {
-        if (!res.locals.user.isAdmin) throw new UnauthorizedError();
+        if (!res.locals.user || !res.locals.user.isAdmin)
+            throw new UnauthorizedError();
         return next();
     } catch (err) {
         return next(err);
